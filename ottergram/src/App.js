75b3be2958adc4edb21293d961440f706e7e0b54
steadyspring <robertayling927@gmail.com>
2024-03-30 16:32:51 +0000

    chapter 2 - removed chapter 1 challenge over to branch

--- ottergram/src/App.js
+++ ottergram/src/App.js
@@ -1,49 +1,33 @@
 import './App.css';
+import Header from './components/Header'
+import Post from './components/Post'
 import Barry from './otters/otter1.jpg'
 import Robin from './otters/otter2.jpg'
 import Maurice from './otters/otter3.jpg'
 import Lesley from './otters/otter4.jpg'
 import Barbara from './otters/otter5.jpg'
 
+const ottersArray = [
+  { image: Barry, name: 'Barry', id: 1 },
+  { image: Robin, name: 'Robin', id: 2 },
+  { image: Maurice, name: 'Maurice', id: 3 },
+  { image: Lesley, name: 'Lesley', id: 4 },
+  { image: Barbara, name: 'Barbara', id: 5 },
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className='header-component'>
-        <h1> Ottergram</h1>
-        <h3>Ottergram is a place to share your favourite otters</h3>
-      </header>
+      <Header></Header>
       <ul className='post-list'>
-        <li className='post-list'>
-          <button>
-            <img src={Barry} alt='Barry'/>
-            <p>Barry</p>
-          </button>
-        </li>
-        <li >
-          <button>
-            <img src={Robin} alt='Robin'/>
-            <p>Robin</p>
-          </button>
-        </li>
-        <li className='post-list'>
-          <button>
-            <img src={Maurice} alt='Maurice'/>
-            <p>Maurice</p>
-          </button>
-        </li>
-        <li className='post-list'>
-          <button>
-            <img src={Lesley} alt='Lesley'/>
-            <p>Lesley</p>
-          </button>
-        </li>
-        <li className='post-list'>
-          <button>
-            <img src={Barbara} alt='Barbara'/>
-            <p>Barbara</p>
-          </button>
-        </li>
+        {ottersArray.map((post) => (
+          <Post
+            key={post.id}
+            image={post.image}
+            name={post.name}
+          ></Post>
+        ))}
       </ul>
     </div>
   );
---
+++ ottergram/src/components/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+    return (
+        <header className='header-component'>
+            <h1>Ottergram</h1>
+        </header>
+    )
+}
+export default Header;
